3ba4a97d92a2fd371b3211d380d320d8c210e10d
vietcuong97 <levietcuong0297@gmail.com>
2022-05-20 17:27:49 +0700

    Update token history by event marketplace

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,17 @@
 export enum TOKEN_ACTION {
-  SELL,
-  BUY,
-  CANCEL,
-  BURN,
-  SWAP,
+  SELL = "Sell",
+  BUY = "Buy",
+  CANCEL = "Cancel",
+  BURN = "Burn",
+  SEND_CLAIM = "SendClaim",
+  SEND_SWAP = "SendSwap",
+  BURN_SWAP = "BurnSwap",
+  TRANSFER_SWAP = "TransferSwap",
+  OPEN_BOX = "OpenBox",
+}
+
+export enum TRANSACTION_TYPE {
+  SELL = "Sell",
+  BUY = "Buy",
+  CANCEL = "Cancel",
 }
